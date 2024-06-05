37527fe1fa7d35e419c4d921ecc132b3ec04acbd
Garus05 <leo-carro020@hotmail.com>
2024-06-05 16:22:17 -0400

    Added comments for each function

--- Deque/Deque.java
+++ Deque/Deque.java
@@ -6,49 +6,59 @@ public class Deque<Item> implements Iterable<Item>{
     private Node front, back;
     private int N;
 
+    // Inner class to implement the nodes of the linked-list.
     private class Node {
         Item item;
         Node next;
         Node previous;
     }
     
+    // Construct an empty deque.
     public Deque(){
         this.front = null;
         this.back = null;
         this.N = 0;
     }
 
+    // Is the deque empty?
     public boolean isEmpty(){
-        return this.front == null;
+        return N == 0;
     }
 
+    // Return the number of items on the deque
     public int size(){
-        return this.N;
+        return N;
     }
 
+    // Add the item to the front
     public void addFirst(Item item){
-        Node oldFront = this.front;
-        this.front = new Node();
-        this.front.item = item;
-        this.front.next = oldFront;
+        Node oldFront = front;
+        front = new Node();
+        front.item = item;
+        front.next = oldFront;
     }
 
+    // Add the item to the back
     public void addLast(Item item){
         Node oldBack = this.back;
     }
 
+    // Remove and return the item from the front
     public Item removeFirst(){
         return null;
     }
 
+    // Remove and return the item from the back
     public Item removeLast(){
         return null;
     }
 
+    // Return an iterator over items in order from front to back
     public Iterator<Item> iterator(){
         return null;
     }
 
+    // Testing unit
     public static void main(String[] args){
 
     }
